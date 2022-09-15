e7135eb3f6094553877049169d8c0f021d7f76bb
Abdeldjalil Hadjnaasse <extern.abdeldjalil.hadjnaasse@europcar.com>
2022-09-15 14:27:11 +0200

    update plugin name

--- spec/google-recaptcha/01-unit_spec.lua
+++ spec/google-recaptcha/01-unit_spec.lua
@@ -1,4 +1,4 @@
-local PLUGIN_NAME = "myplugin"
+local PLUGIN_NAME = "google-recaptcha"
 
 
 -- helper function to validate data against a schema
--- spec/google-recaptcha/02-integration_spec.lua
+++ spec/google-recaptcha/02-integration_spec.lua
@@ -1,7 +1,7 @@
 local helpers = require "spec.helpers"
 
 
-local PLUGIN_NAME = "myplugin"
+local PLUGIN_NAME = "google-recaptcha"
 
 
 for _, strategy in helpers.all_strategies() do
