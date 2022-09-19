4405ab894897cdd97d398c2df3208474bbbae857
Abdeldjalil <hn.abdeldjalil@gmaiil.com>
2022-09-19 15:22:43 +0200

    TESTING PLUGIN INTEGRATION
    * add plugin configs to users routes
    * add logs in the handler.lua/plugin:access

--- kong/plugins/google-recaptcha/handler.lua
+++ kong/plugins/google-recaptcha/handler.lua
@@ -8,14 +8,16 @@ local plugin = {
   VERSION = "0.1.0", -- version in X.Y.Z format. Check hybrid-mode compatibility requirements.
 }
 
+-- kong global variable
+-- https://docs.konghq.com/gateway/latest/plugin-development/pdk/
 local kong = kong
 
 -- runs in the 'access_by_lua_block'
 function plugin:access(plugin_conf)
 
   -- your custom code here
-  kong.log.inspect(plugin_conf)   -- check the logs for a pretty-printed config!
-  kong.service.request.set_header(plugin_conf.request_header, "this is on a request")
+  -- kong.log.inspect(plugin_conf)   -- check the logs for a pretty-printed config!
+  kong.log("recaptcha plugin", "recaptcha plugin")
 
 end --]]
 
