7763bfa454b9c7deac0cc38586944edb66cd4cdb
Abdeldjalil <hn.abdeldjalil@gmaiil.com>
2022-09-19 17:07:41 +0200

    TESTING PLUGIN INTEGRATION

--- kong/plugins/google-recaptcha/handler.lua
+++ kong/plugins/google-recaptcha/handler.lua
@@ -12,70 +12,10 @@ local plugin = {
 -- https://docs.konghq.com/gateway/latest/plugin-development/pdk/
 local kong = kong
 
-kong.log("recaptcha plugin before", "recaptcha plugin before");
-
-function plugin:init_worker()
-  -- Implement logic for the init_worker phase here (http/stream)
-  kong.log.debug("init_worker")
-end
-
-
-function plugin:preread(config)
-  -- Implement logic for the preread phase here (stream)
-  kong.log.debug("preread")
-end
-
-
-function plugin:certificate(config)
-  -- Implement logic for the certificate phase here (http/stream)
-  kong.log.debug("certificate")
-end
-
-function plugin:rewrite(config)
-  -- Implement logic for the rewrite phase here (http)
-  kong.log.debug("rewrite")
-end
-
 function plugin:access(config)
   -- Implement logic for the access phase here (http)
   kong.log.debug("access")
 end
-
-function plugin:ws_handshake(config)
-  -- Implement logic for the WebSocket handshake here
-  kong.log.debug("ws_handshake")
-end
-
-function plugin:header_filter(config)
-  -- Implement logic for the header_filter phase here (http)
-  kong.log.debug("header_filter")
-end
-
-function plugin:ws_client_frame(config)
-  -- Implement logic for WebSocket client messages here
-  kong.log.debug("ws_client_frame")
-end
-
-function plugin:ws_upstream_frame(config)
-  -- Implement logic for WebSocket upstream messages here
-  kong.log.debug("ws_upstream_frame")
-end
-
-function plugin:body_filter(config)
-  -- Implement logic for the body_filter phase here (http)
-  kong.log.debug("body_filter")
-end
-
-function plugin:log(config)
-  -- Implement logic for the log phase here (http/stream)
-  kong.log.debug("log")
-end
-
-function plugin:ws_close(config)
-  -- Implement logic for WebSocket post-connection here
-  kong.log.debug("ws_close")
-end
-
 
 -- return our plugin object
 return plugin
