9103ad297963d46d73cb8b74860836fc1c58b627
Abdeldjalil <hn.abdeldjalil@gmaiil.com>
2022-09-19 16:10:04 +0200

    TESTING PLUGIN INTEGRATION

--- kong/plugins/google-recaptcha/schema.lua
+++ kong/plugins/google-recaptcha/schema.lua
@@ -39,4 +39,27 @@ local schema = {
   },
 }
 
-return schema
+return {
+  name = PLUGIN_NAME,
+  fields = {
+    {
+      -- this plugin will only be applied to Services or Routes
+      consumer = typedefs.no_consumer
+    },
+    {
+      -- this plugin will only run within Nginx HTTP module
+      protocols = typedefs.protocols_http
+    },
+    {
+      config = {
+        type = "record",
+        fields = {
+          -- Describe your plugin's configuration's schema here.
+        },
+      },
+    },
+  },
+  entity_checks = {
+    -- Describe your plugin's entity validation rules
+  },
+}
