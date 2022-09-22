8ba2f5841c97513abc2ca44b218ef7924967b2ba
Abdeldjalil <hn.abdeldjalil@gmaiil.com>
2022-09-22 16:28:45 +0200

    update kong-plugin-google-recaptcha-0.1.0-1.rockspec

--- kong/plugins/google-recaptcha/handler.lua
+++ kong/plugins/google-recaptcha/handler.lua
@@ -47,7 +47,8 @@ function valid(secret_key, g_captcha_res, remote_ip)
     source = ltn12.source.string(request_body),
     sink = ltn12.sink.table(response_body)
   }
-
+  kong.log.inspect(response_body)
+  response_body = json.decode(response_body)
   kong.log.inspect(response_body)
   kong.log.inspect(r)
   kong.log.inspect(c)
